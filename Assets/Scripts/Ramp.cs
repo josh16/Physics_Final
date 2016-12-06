2cf57299187577ea063d6ca9f750cc0ae78b735d
MatthewRadeff <matt_radeff@hotmail.com>
2016-12-06 13:33:03 -0500

    level 2 shit
    ramps, platforms

---
+++ Assets/Scripts/Ramp.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ramp : MonoBehaviour
+{
+
+	void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            other.rigidbody.AddForce(new Vector3(1200.0f, 700.0f, 0.0f));
+        }
+    }
+
+
+
+}
