6427427f2df597de1930375da2d85ed64cb4e1cf
Josh.R <Josh@Joshs-Macbook-Pro-2.local>
2016-12-06 13:39:02 -0500

    Rotation Script
    -Added rotation script
    -minor changes

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour 
+{
+
+	public float Rotationspeed;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		transform.Rotate(0, Rotationspeed,0);
+	}
+}
