efd133ce5a4f3c770a48de370fdbfcde3f190252
Josh.R <Josh@Joshs-Macbook-Pro-2.local>
2016-12-06 23:18:59 -0500

    Extra Stuff
    Extra Stuff

---
+++ Assets/Scripts/FallingPlatform.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingPlatform : MonoBehaviour
+{
+    private GameObject other;
+
+    void OnCollisionEnter(Collision col)
+    {
+        
+
+        if (col.gameObject.CompareTag("Platform"))
+        { 
+            this.other = col.gameObject;
+            StartCoroutine(FallingObject());
+        }
+    }
+
+    IEnumerator FallingObject()
+    {
+        yield return new WaitForSeconds(0.75f);
+        this.other.GetComponent<Rigidbody>().isKinematic = false;
+        this.other.GetComponent<Rigidbody>().useGravity = true;
+    }
+
+
+}
---
+++ Assets/Scripts/ramp2.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ramp2 : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.rigidbody.AddForce(new Vector3(-1500.0f, 400.0f, 0.0f));
+        }
+    }
+
+}
